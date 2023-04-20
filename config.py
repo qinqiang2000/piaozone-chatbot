e3855e3935390555ce34569c2c9dbf8f8e2161f4
qinqiang2000 <qinqiang2000@qq.com>
2023-04-20 16:16:44 +0800

    v0.13

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', "n7NLbcATELBcuVILYiIWT3BlbkFJx90IXIuX9uHhWBuTBGyx")
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', "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")
 INDEX_NAME = "fpy_operation_qa"
 REDIS_URL = "redis://localhost:6379"
 FAISS_DB_PATH = "db"

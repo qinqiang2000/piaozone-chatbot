f5166f82b1c1b56f898a3048de4ae1f993bb9ace
qinqiang2000 <qinqiang2000@qq.com>
2023-04-20 16:09:12 +0800

    v0.12

--- chat_yunzhijia.py
+++ chat_yunzhijia.py
@@ -75,4 +75,4 @@ async def chat(msg: RobotMsg, task: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=80)

d15af9031500675372efc1e8820a903e7bce3ac8
maxie <wenwei_yang@kingdee.com>
2023-12-05 02:43:50 +0800

    添加pdf文件转化为html的方法

--- common_utils.py
+++ common_utils.py
@@ -3,6 +3,10 @@ import logging
 from settings import CONFIG_PATH, YZJ_ASSISTANT_RELATE_PATH
 import json
 from bs4 import BeautifulSoup
+import mammoth
+import os
+from pdf2docx.converter import Converter
+from docx import Document
 
 
 def get_assistant_id_by_yzj_token(yzj_token):
@@ -41,3 +45,39 @@ def clear_css_code(html_content):
     for tag in soup.find_all(class_=True):
         del tag['class']
     return soup.prettify()
+
+
+def convert_pdf_to_docx(pdf_path, filename):
+    """
+    将pdf文件转化为docx文件，并指定为filename
+    :param pdf_path:
+    :param filename:
+    :return:
+    """
+    docx_filename = os.path.join(os.path.dirname(pdf_path), filename + '.docx')
+
+    cv = Converter(pdf_path)
+    cv.convert(docx_filename=docx_filename, ignore_page_error=False)
+    cv.close()
+    return docx_filename, count_words_in_docx(docx_filename)
+
+
+def count_words_in_docx(docx_filename):
+    doc = Document(docx_filename)
+    total_words = 0
+    for para in doc.paragraphs:
+        total_words += len(para.text.split())
+    return total_words
+
+
+def get_docx_html_content(docx_file_path):
+    """
+    将docx文件转化为html内容,并返回
+    :param docx_file_path:
+    :return:
+    """
+    print(f"将文件{docx_file_path}转换为html内容")
+    with open(docx_file_path, "rb", encoding="utf-8") as docx_file:
+        result = mammoth.convert_to_html(docx_file)
+        html_content = result.value  # 生成的 HTML 内容
+        return html_content

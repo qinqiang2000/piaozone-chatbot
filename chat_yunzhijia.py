faa443b397afb9d6e907820dd492279b8fe4e498
maxie <wenwei_yang@kingdee.com>
2023-12-15 14:48:54 +0800

    添加图片以云之家卡片消息推送逻辑

--- chat_yunzhijia.py
+++ chat_yunzhijia.py
@@ -88,7 +88,10 @@ def send_yzj_card_notice(yzj_token, img_urls, operator_open_id):
     if not img_urls:
         return
     img_num = len(img_urls)
-    card_num = img_num / MAX_IMG_NUM_IN_CARD_NOTICE + 1
+    card_num = img_num / MAX_IMG_NUM_IN_CARD_NOTICE
+    # 图片数量不能被MAX_IMG_NUM_IN_CARD_NOTICE整除，则需要卡片数+1
+    if img_num % MAX_IMG_NUM_IN_CARD_NOTICE != 0:
+        card_num += 1
     for i in range(card_num):
         data_content = gen_card_notice_data_content(img_urls, img_num, card_num, i)
         # 卡片填充信息
